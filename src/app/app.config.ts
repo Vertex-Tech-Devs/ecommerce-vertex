1a532ff74ce5a53a104a61009be9bcaa8308b23b
Juan Leonidas Espeche <juan.l.espeche@gmail.com>
2026-06-22 00:26:29 -0300

    fix: use ModalModule.forRoot() to resolve RendererFactory2 dependency injection error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -82,7 +82,7 @@ export function createAppConfig(firebaseConfig: FirebaseOptions): ApplicationCon
       }),
       provideStorage(() => getStorage()),
 
-      importProvidersFrom(ModalModule),
+      importProvidersFrom(ModalModule.forRoot()),
 
       {
         provide: APP_INITIALIZER,
