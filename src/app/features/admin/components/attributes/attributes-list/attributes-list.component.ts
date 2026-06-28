4817748ef60454351f744d3dd98ec52128cd2f3b
Lihue Leiva <leivalihue@gmail.com>
2026-06-28 03:40:10 -0300

    feat(attributes): implement autofocus on name input and dynamic formarray entries

--- src/app/features/admin/components/attributes/attributes-list/attributes-list.component.ts
+++ src/app/features/admin/components/attributes/attributes-list/attributes-list.component.ts
@@ -2,8 +2,7 @@ import type { OnInit, OnDestroy } from '@angular/core';
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import type { Observable, Subscription } from 'rxjs';
-// eslint-disable-next-line @typescript-eslint/consistent-type-imports -- DI token requires runtime import
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import type { BsModalRef } from 'ngx-bootstrap/modal';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import type { Attribute } from '@core/models/attribute.model';
 import { AttributeService } from '@core/services/attribute.service';
