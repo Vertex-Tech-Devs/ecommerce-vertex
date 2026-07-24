fe7ca491a42fa051410370ec1ffddbf9e51d7f8e
Juan Leonidas Espeche <juan.l.espeche@gmail.com>
2026-07-24 13:28:21 -0300

    chore(eslint): clean lint warnings in storefront

--- src/app/features/admin/components/products/product-create/product-create.component.ts
+++ src/app/features/admin/components/products/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import type { OnInit, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import { Component, inject, ViewChildren, DestroyRef, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
